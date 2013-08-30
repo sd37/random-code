1330b143afb9907a66394eafebb1f791c372b8ce
spandan <spandan@sd37.(none)>
2013-08-30 19:32:09 -0400

    working on priority problem

---
+++ rq/priority.cpp
@@ -0,0 +1,24 @@
+//http://www.talentbuddy.co/challenge/51ce446a4af0110af3826346
+//backtracking
+
+#include <iostream>
+#include <vector>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+using namespace std;
+
+void count_configurations(int a,int b, int c,int n)
+{
+	
+}
+int main()
+{
+	int a,b,c,n;
+	a = 2, b = 2 , c = 2 , n = 2;
+	
+	count_configurations(a,b,c,n);
+	
+	return 0;	
+}
