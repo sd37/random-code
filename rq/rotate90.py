271eda4128bb77746f80f6cafdd0d7cb22f27b7f
spandan <spandan@sd37.(none)>
2013-09-01 18:43:52 -0400

    rotate90.py added

---
+++ rq/rotate90.py
@@ -0,0 +1,44 @@
+import re
+def print_l(r):
+    s = str(r)
+    s = re.sub(","," ",s)
+    s = s[1:len(s) - 1]
+    print s
+def display(arr):
+    for r in arr:
+	print_l(r)
+    
+def create_2darray(lines):
+    dim = len(lines)
+    Matrix = [[0  for x in xrange(dim)] for x in xrange(dim)]
+    for i in range(dim):
+	tkns = lines[i].split(",")
+	for j in range(dim):
+	    Matrix[i][j] = int(tkns[j])
+    return Matrix	    	
+	
+def trp(arr):
+    dim = len(arr[0])
+    for i in range(dim-1):
+	for j in range(i + 1,dim):
+	    temp = arr[i][j]
+	    arr[i][j] = arr[j][i]
+	    arr[j][i] = temp
+    return arr
+def rev_col(arr):
+    i = 0
+    while i < len(arr[0]):
+	arr[i].reverse()
+	i = i + 1
+    return arr
+
+def rotate_matrix(lines):
+    arr2d = create_2darray(lines)
+    arr2d  = trp(arr2d) 	
+    arr2d = rev_col(arr2d)
+    display(arr2d)
+
+lines = ["1,2,3","4,5,6","7,8,9"]
+rotate_matrix(lines)
+
+
