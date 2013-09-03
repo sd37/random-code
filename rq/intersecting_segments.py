b8236fbbfcd0d78eccf889d188236a98c34f8c95
spandan <spandan@sd37.(none)>
2013-09-02 23:43:41 -0400

    intersecting_segments.py

--- rq/intersecting_segments.py
+++ rq/intersecting_segments.py
@@ -1,3 +1,11 @@
+'''
+def tuple_to_str(t):
+    t1 = t[0]
+    t2 = t[1]
+
+    s = str(t1[0]) + "," + str(t2[0]) + "," + str(t2[0]) + "," + str(t2[1])
+    return s
+'''    
 def construct_2darray(seg,p):
     Matrix = [[0 for x in xrange(2)] for x in xrange(2)]
     Matrix[0][0] = seg.getp1().getX() - p.getX()
@@ -24,6 +32,7 @@ class Point(object):
 
     def setY(self,y):
 	self.y = y
+
     def getX(self):
 	return self.x
     
@@ -34,16 +43,23 @@ class Segment(object):
     def __init__(self,p1,p2):
 	self.p1 = p1
 	self.p2 = p2
-    def __repr__(self):
-	s = str(self)
-	return s
+    
     def getp1(self):
 	return self.p1
     
     def getp2(self):
 	return self.p2
 
+    def setp1(self,p1):
+	self.p1 = p1
 
+    def setp2(self,p2):
+	self.p2 = p2
+
+    def __repr__(self):
+	return str((self.p1,self.p2))
+
+    
     def doIntersect(self,seg):
 	arr2d1 = construct_2darray(self,seg.getp1())
 	sign1  = compute_determinant(arr2d1)
@@ -54,7 +70,6 @@ class Segment(object):
 	    return True
 	else:
 	    return False
-
 def createSegment(seg):
     ls = seg.split(",")
     p1 = Point(0,0)
@@ -99,4 +114,4 @@ def intersecting_segments(segments):
 	print dd[str(createSegment(x))]
 	
     
-intersecting_segments(["0,1,2,1","1,0,1,2"])
+intersecting_segments(['1,1,2,5', '1,1,5,4', '5,4,6,1', '6,1,1,1', '2,5,5,4'])
