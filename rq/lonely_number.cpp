cc421e4c45c494fce96b3b75a41b2e9c825ce296
spandan <spandan@sd37.(none)>
2013-10-18 17:19:35 -0400

    lonely_number.cpp added

---
+++ rq/lonely_number.cpp
@@ -0,0 +1,31 @@
+//https://www.hackerrank.com/challenges/lonely-integer
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int single_element(vector<int>& v)
+{
+    int ans = 0;
+    for (int i = 0; i < v.size(); i++)
+        ans ^= v[i];
+
+    return ans;
+}
+int main() {
+    vector<int> v;
+    int n;
+    cin >> n;
+    
+    while(n--)
+    {
+        int x;
+        cin >> x;
+        v.push_back(x);
+    }
+
+    cout << single_element(v);
+    return 0;
+}
