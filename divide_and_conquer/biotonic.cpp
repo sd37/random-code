39b2656991fab69e8ece72d5dbac26952ad6139c
spandan <spandan@sd37.(none)>
2013-11-03 20:23:51 -0500

    biotonic.cpp added

---
+++ divide_and_conquer/biotonic.cpp
@@ -0,0 +1,42 @@
+//assume distinct integers in the array
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int find_pivot(vector<int>& v,int low,int high)
+{
+    while(low <= high)
+    {
+        int mid = (high - low) / 2 + low;
+        
+        if( (mid == 0 || mid == v.size() -1) || (v[mid - 1] < v[mid] && v[mid] > v[mid + 1]))
+            return mid;
+        else if (v[mid - 1] < v[mid])
+        {
+            low = mid + 1;
+        }
+        else 
+        {
+            high = mid - 1;
+        }
+    }
+}
+
+int find_max(vector<int>&v)
+{
+    int pivot = find_pivot(v,0,v.size() -1);
+    
+    return v[pivot];
+}
+int main()
+{
+    //vector<int> v({8,10,20,80,100,200,400,500,3,2,1});
+    //vector<int> v({1,3,50,10,9,7,6});
+    //vector<int> v({10,20,30,40,50});
+    //vector<int> v({120,100,80,20,0});
+    vector<int> v({1,30,40,50,60,70,23,20});
+    int num = find_max(v);
+    cout << num << endl;
+    return 0;
+}
