f537664f27afb62b30fd6156fdd63556de9bcdea
spandan <spandan@sd37.(none)>
2013-11-17 20:15:04 -0500

    min_element_updated

--- divide_and_conquer/min_element.cpp
+++ divide_and_conquer/min_element.cpp
@@ -9,8 +9,8 @@ int find_min(vector<int>& v,int low,int high)
     while(low <= high)
     {
         int mid = (high - low) / 2 + low;
-
-        if( v.size() == 1 ||  (mid > 0 && v[mid-1] > v[mid]))
+        
+        if(mid > 0 && v[mid-1] > v[mid])
             return mid;
         
         else if(mid > 0 && v[mid] < v[0]) 
@@ -28,10 +28,10 @@ int find_min(vector<int>& v,int low,int high)
 
 int main()
 {
-    vector<int> v({5,6,7,8,9,10,11,1,2,3,4});
+    //vector<int> v({5,6,7,8,9,10,11,1,2,3,4});
     //vector<int> v({1,2,3,4,5,6});
     //vector<int> v({3,4,5,6,2});
-    //vector<int> v{1,2};
+    vector<int> v{1,2};
     //vector<int> v{2,1};
     //vector<int> v({4,5,6,7,1,2,3});
     int min_index = find_min(v,0,v.size()-1);
