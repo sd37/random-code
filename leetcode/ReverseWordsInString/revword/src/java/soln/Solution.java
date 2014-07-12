d1bb7d9dc4b2b2f4ffcc3f8a533a759d1d34ebcd
spandand <spandand@cloudera.com>
2014-07-12 14:52:54 -0700

    revwords.java added

---
+++ leetcode/ReverseWordsInString/revword/src/java/soln/Solution.java
@@ -0,0 +1,33 @@
+package soln;
+
+public class Solution {
+    public String reverseWords(String s) {
+        StringBuilder sb = new StringBuilder(s);
+
+        sb.reverse();
+
+        String[] parts = sb.toString().split("\\s+");
+        
+        String new_s = "";
+
+
+        for(String it : parts)
+        {
+            StringBuilder temp_sb = new StringBuilder(it);
+            temp_sb.reverse();
+            new_s += temp_sb + " ";
+        }
+       
+        return new_s.substring(0, new_s.length() - 1);
+    }
+
+    public static final void main(String[] args) {
+        String s = "   the   sky is   blue";
+
+        Solution soln = new Solution();
+
+        String ans_s = soln.reverseWords(s);
+
+        System.out.println(ans_s);
+    }
+}
