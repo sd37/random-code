64c851d9a24f523dfebc95f86ad4df9cf6bd2748
spandand <spandand@cloudera.com>
2014-07-27 13:41:43 -0700

    problem5 added

---
+++ euler/problem5/problem5.py
@@ -0,0 +1,60 @@
+# https://projecteuler.net/problem=5
+# status = AC
+
+import sys
+
+def primeFactors(N):
+
+    i = 2
+    factors = []
+
+    while N > 1:
+        
+        while (N % i == 0):
+            factors.append(i)        
+            N = N / i
+        
+        i = i + 1
+
+        if i*i > N:
+            if N > 1:
+                factors.append(N)
+            break
+
+    primed = dict()
+
+    for p in factors:
+        
+        if p not in primed:
+            primed[p] = 1
+        else:
+            primed[p] = primed[p] + 1
+
+    return primed
+
+def lcm(ln):
+    
+    assert(len(ln) >= 2)
+    
+    lcm_prime = dict()
+
+    for x in ln:
+        primed = primeFactors(x)
+        
+        for p in primed:
+
+            if p not in lcm_prime:
+                lcm_prime[p] = primed[p]
+            else:
+                if lcm_prime[p] < primed[p]:
+                    lcm_prime[p] = primed[p]
+
+    ans = 1
+
+    for p in lcm_prime:
+        ans = ans * (p ** lcm_prime[p])
+
+    return ans
+
+ln = range(1,20 + 1)
+print lcm(ln)
