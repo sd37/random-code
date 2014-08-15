8f1317ec0be2d97073455929c84a4f965fd3f949
spandand <spandand@cloudera.com>
2014-08-14 20:50:54 -0700

    srm502div2added

--- tc/srm/SRM 502 DIV 2/src/TheProgrammingContestDivTwo.java
+++ tc/srm/SRM 502 DIV 2/src/TheProgrammingContestDivTwo.java
@@ -5,6 +5,29 @@ import static java.lang.Math.*;
 public class TheProgrammingContestDivTwo {
 	
 	public int[] find(int T, int[] requiredTime) {
-		return null;
+
+        int[] score = new int[2];
+
+        score[0] = 0; // solved
+        score[1] = 0; // penalty
+
+        Arrays.sort(requiredTime);
+
+        int curr_time = 0;
+
+        for (int i = 0; i < requiredTime.length; i++)
+        {
+            if (requiredTime[i] <= T)
+            {
+                score[0]++;
+                curr_time += requiredTime[i];
+                score[1]+= curr_time;
+                T -= requiredTime[i];
+            }
+            else
+                break;
+        }
+
+		return score;
 	}
 }
