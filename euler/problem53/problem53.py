16d4ccecdf3ba74bb36214af4685ff1808d090dc
spandand <spandand@cloudera.com>
2014-09-15 21:13:13 -0700

    problem53.py adde

---
+++ euler/problem53/problem53.py
@@ -0,0 +1,21 @@
+import sys
+import math
+
+def compute_nCr(n,r):
+    ans = math.factorial(n) / (math.factorial(r) * math.factorial(n-r))
+    return ans
+
+def solve():
+    n = 1
+    c = 0;
+    while(n <= 100):
+        r = 0
+        while r <= n:
+            if compute_nCr(n,r) > 1000000:
+                c = c + 1
+            r = r + 1
+        n = n + 1
+
+    print c
+
+solve()
